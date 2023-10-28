44460e073786e8514c2a5511637ae0945cfd1421
Zguedest <zucelys.zge@gmail.com>
2023-10-28 23:09:34 +0200

    MiUniversidad por Universidad

--- Universidad/wsgi.py
+++ Universidad/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for MiUniversidad project.
+WSGI config for Universidad project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MiUniversidad.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Universidad.settings')
 
 application = get_wsgi_application()
