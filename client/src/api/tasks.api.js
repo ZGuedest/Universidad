5ad2a66c5e9bde34c5b1f0730648af01e7e93ef5
Zguedest <zucelys.zge@gmail.com>
2023-10-29 20:41:03 +0100

    creando tareas

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,5 +1,18 @@
 import axios from 'axios'
 
-export const getAllTasks = () =>{
-    return axios.get('http://localhost:8000/tasks/api/v1/tasks/')
-}
+const taskApi = axios.create({
+    baseURL: 'http://localhost:8000/tasks/api/v1/tasks/'
+})
+
+// export const getAllTasks = () =>{
+//     return taskApi.get('/')
+// }
+
+// export const createTask = (task) =>{
+//     return taskApi.post('/',task)
+// }
+
+ //forma mas brave de escribir lo mismo q arriba 
+ 
+export const getAllTasks = () =>  taskApi.get('/')
+export const createTask = (task) => taskApi.post('/',task)
